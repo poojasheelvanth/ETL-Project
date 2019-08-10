a1152ca8f18392a12120032311f0dbf0c0e546dc
unknown <peterpark2@gmail.com>
2019-08-10 09:48:02 -0500

    revised notebook

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -1,28 +1,43 @@
-DROP TABLE starbucks;
-DROP TABLE happiness;
+DROP TABLE IF EXISTS starbucks;
+DROP TABLE IF EXISTS happiness;
+
+--
+
 CREATE TABLE starbucks (
-    id TEXT PRIMARY KEY,
-    brand TEXT,
-    store_name TEXT,
-    ownership_type TEXT,
-    address TEXT,
-    city TEXT,
-    state TEXT,
-    country TEXT
+    id TEXT PRIMARY KEY
+    , brand TEXT
+    , store_name TEXT
+    , ownership_type TEXT
+    , address TEXT
+    , city TEXT
+    , state TEXT
+    , country TEXT
 );
 
- SELECT * from starbucks;
+SELECT * from starbucks;
 
-CREATE TABLE happiness (
-    id SERIAL,
-    country TEXT,
-    happiness INT
+--
+
+CREATE TABLE happiness(
+    id SERIAL
+    , country TEXT
+    , happiness INT
 );
- SELECT * from happiness;
+
+SELECT * from happiness;
+
+
 -- Joins tables
-SELECT starbucks.brand, starbucks.country, COUNT(starbucks.id) AS "Number Of Stores", happiness.happiness
-FROM starbucks
-JOIN happiness
-ON starbucks.country = happiness.country
-group by starbucks.brand, starbucks.country, happiness.happiness
-order by happiness.happiness;
+SELECT 
+	star.brand
+	, star.country
+	, COUNT(star.id) AS "Number Of Stores"
+	, hap.happiness
+FROM star
+JOIN hap
+	ON star.country = hap.country
+GROUP BY 
+	star.brand
+	, star.country
+	, hap.happiness
+ORDER BY hap.happiness;
